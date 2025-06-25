b299018ea314b37db7641af27a5ed8855165c114
Rudra <rudra25parmeshwar@gmail.com>
2025-06-25 15:21:48 +0530

    testing done

--- src/utils/gitHubTest.js
+++
@@ -1,9 +0,0 @@
-const quotes = [
-    "Stay hungry, stay foolish.",
-    "Code is like humor. When you have to explain it, it’s bad.",
-    "First, solve the problem. Then, write the code."
-  ];
-  
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  console.log("Quote of the day:", quotes[randomIndex]);
-  
